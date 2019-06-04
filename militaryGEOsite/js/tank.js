d82bb719d9778989db035888a4933f7f9664e785
mdvitalie <mdvitalie@gmail.com>
2019-06-04 13:35:28 +0100

    fixed tank.js file

--- militaryGEOsite/js/tank.js
+++ militaryGEOsite/js/tank.js
@@ -8,15 +8,15 @@
 // ];
 
 let tank = [
-    ['Country', 'ship'],
-    ['Russia', 21932]
+    ['Country', 'tank'],
+    ['Russia', 21932],
     ['China', 13050],
     ['United States', 6287],
     ['North Korea', 6075],
     ['Syria', 5035]
 
     // ['Country', 'ship'],
-    // ['North Korea', 967],
+    // ['North Korea', 20967],
     // ['China', 714],
     // ['United States', 415],
     // ['Iran', 398],
